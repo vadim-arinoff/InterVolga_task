a6ad1ba27b3a3b71fd59424316ca50359344a812
vadim-arinoff <carralfull@gmail.com>
2025-05-27 14:06:43 +0300

    Добавил ссылки в header

--- task1/header.php
+++ task1/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Таблица</title>
-
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script defer src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script defer src="form-handler.js"></script>
